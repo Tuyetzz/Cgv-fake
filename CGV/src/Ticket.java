28bd3cdcc67a1d8d002aedd3a3939856f091134a
Tuyetzz <128302380+Tuyetzz@users.noreply.github.com>
2023-11-11 17:34:03 +0700

    Add files via upload

---
+++ CGV/src/Ticket.java
@@ -0,0 +1,47 @@
+//package com.mycompany.netbeanstest2;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Ticket {
+    private Date date;
+    private String name;
+    private int price;
+    private int seat;
+    Ticket() {
+
+    }
+    public int getSeat() {
+        return seat;
+    }
+
+    public void setSeat(int seat) {
+        this.seat = seat;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+}
