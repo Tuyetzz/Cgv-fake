ece8bdb7f307b99c8810e532b9d2d4e6541891c2
YeLing15 <95345279+YeLing15@users.noreply.github.com>
2023-11-13 00:29:43 +0700

    Update MovieManagementSystemGUI.java

--- CGV/src/MovieManagementSystemGUI.java
+++ CGV/src/MovieManagementSystemGUI.java
@@ -11,7 +11,7 @@ import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
-public class MovieManagementSystemGUI {
+public class MovieManagementSystemGUI extends JFrame {
     private ArrayList<Movie> movieList = new ArrayList<>();
     private DefaultListModel<Movie> movieListModel = new DefaultListModel<>();
     private JList<Movie> movieJList = new JList<>(movieListModel);
